bd38702ec7c1444a2983714f6582945905d23a94
donghao <hao.dong@bca.network>
2022-07-13 18:19:58 +0800

    fix: v0.0.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name='common_tool',
-    version='0.0.2',
+    version='0.0.3',
     author='donghao',
     author_email=AUTHOR_EMAIL,
     url="https://github.com/RelaxedDong/dh_common",
